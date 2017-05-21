9db0e4fe47d846e14f896095c0fb2a8ea9c2b5c3
Robin van Vlijmen <Robin van Vlijmen@192.168.178.15>
2017-05-21 12:46:33 +0200

    getdocentNaam functie gemaakt

--- src/Docent.java
+++ src/Docent.java
@@ -4,10 +4,10 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Docent {
-	
+
 	public static void main(String argv[]) {
 		try {
-			Document doc = Utils.getXMLDoc("res/docent.xml"); //CHANGE TO YOUR OWN DIRECTORY //Requires method to find it dynamically.
+			Document doc = Utils.getXMLDoc("res/docent.xml"); 
 			NodeList nList = doc.getElementsByTagName("docent");
 			System.out.println("TABLE NAME: docenten \n");
 
@@ -18,9 +18,30 @@ public class Docent {
 					System.out.println("docent ID: " + eElement.getAttribute("id"));
 				}
 			}
-		} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String getdocentNaam(int docentID) {
+		try {
+			Document doc = Utils.getXMLDoc("res/docent.xml");
+			NodeList nList = doc.getElementsByTagName("docent");
+
+			for (int item = 0; item < nList.getLength(); item++) {
+				Node nNode = nList.item(item);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					if (Integer.parseInt(eElement.getAttribute("id")) == docentID) {
+						return (eElement.getElementsByTagName("naam").item(0).getTextContent());
+					}
+				}
+			}
+		}
+
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		return ("Geen docent gevonden!");
 	}
 }
